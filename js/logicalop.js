0d9e6788d0b320ba8342589bd35823bfb09a5a55
KSHITIZ <138414277+KSHITIZNIROULA@users.noreply.github.com>
2023-12-09 21:51:35 +0545

    Add files via upload

---
+++ js/logicalop.js
@@ -0,0 +1,9 @@
+let firstNum = 14;
+let secondNum = 5;
+if (firstNum === secondNum) {
+  console.log("Firstnum is equal to secondNum.");
+} else if (firstNum >= secondNum) {
+  console.log("First number is greater.");
+} else {
+  console.log("First number is smaller.");
+}
