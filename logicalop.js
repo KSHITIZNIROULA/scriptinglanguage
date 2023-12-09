f71d5ab8becc84188a227112336db48651f6ab9c
KSHITIZ <138414277+KSHITIZNIROULA@users.noreply.github.com>
2023-12-09 21:42:07 +0545

    Delete logicalop.js

--- logicalop.js
+++
@@ -1,9 +0,0 @@
-let firstNum = 14;
-let secondNum = 5;
-if (firstNum === secondNum) {
-  console.log("Firstnum is equal to secondNum.");
-} else if (firstNum >= secondNum) {
-  console.log("First number is greater.");
-} else {
-  console.log("First number is smaller.");
-}
