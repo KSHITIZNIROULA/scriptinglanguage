ae62fcb11ac4c478b9e0d228016450f789f5a049
KSHITIZNIROULA <niroulachhitiz@gmail.com>
2023-12-10 15:42:55 +0545

    file added

---
+++ switch.js
@@ -0,0 +1,49 @@
+let day = prompt("Enter a day"); // day variable banayem
+
+switch (day) {  //day = sunday rakhem 
+    case "1":
+        {
+        console.log("SUNDAY!");
+        break;
+        }
+    case "2":
+        {
+        console.log("MONDAY");
+        break;
+        }
+    
+    case "3":
+    {
+    console.log("TUESDAY");
+    break;
+            }
+            case "4":
+                {
+                console.log("WEDNESDSAY");
+                break;
+                }
+                case "5":
+                    {
+                    console.log("THURSDAY");
+                    break;
+                    }
+
+                    case "6":
+                        {
+                        console.log("FRIDAY");
+                        break;
+                        }
+
+                        case "7":
+                            {
+                            console.log("SATURDAY");
+                            break;
+                            }
+
+
+
+                                 default:
+                                  {
+                                   console.log("Not a valid day.");
+                                   }
+}
